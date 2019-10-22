0b784a29c880e6f5f10f277c90abb440af8ee13e
Stefaan Samyn <stefaan.samyn@hogent.be>
2019-10-22 18:19:10 +0200

    Add project files

---
+++ SportsStore/Models/Domain/IProductRepository.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace SportsStore.Models.Domain {
+    public interface IProductRepository {
+        IEnumerable<Product> GetAll();
+        Product GetById(int productId);
+        void Add(Product product);
+        void Delete(Product product);
+        void SaveChanges();
+    }
+}
